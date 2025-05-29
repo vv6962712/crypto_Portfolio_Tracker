4ed085f847927068d47c25dcbe5eba23bbd35c13
Hadi <abdulhadimohammedashraf@gmail.com>
2025-05-29 10:08:13 +0530

    [abdul] added base project

--- src/main/java/com/bridgelabz/cryptotracker/user/controller/AuthController.java
+++ src/main/java/com/bridgelabz/cryptotracker/user/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.cryptotracker.user.controller;
 
-import com.bridgelabz.cryptotracker.user.dto.LoginRequest;
-import com.bridgelabz.cryptotracker.user.dto.RegisterRequest;
-import com.bridgelabz.cryptotracker.user.service.UserService;
+//import com.bridgelabz.cryptotracker.user.dto.LoginRequest;
+//import com.bridgelabz.cryptotracker.user.dto.RegisterRequest;
+//import com.bridgelabz.cryptotracker.user.service.UserService;
 import jakarta.validation.Valid;  
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,40 +13,4 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
-    private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
-
-    // registers user and return custom message
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
-        userService.registerUser(
-            request.getName(),
-            request.getUserId(),
-            request.getEmail(),
-            request.getPassword(),
-            request.getRole()
-        );
-        return ResponseEntity.ok("Registered successfully");
-    }
-
-    // admin can access all other users
-    @GetMapping("/users")
-    public List<?> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    // login and return message
-    @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
-        try {
-            String message = userService.loginUser(request.getEmail(), request.getPassword());
-            return ResponseEntity.ok(message);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed: " + e.getMessage());
-        }
-    }
 }
--- src/main/java/com/bridgelabz/cryptotracker/user/scheduler/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.bridgelabz.cryptotracker.user.scheduler;
