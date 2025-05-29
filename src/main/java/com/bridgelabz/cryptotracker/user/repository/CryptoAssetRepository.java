0b824401685de0d0f3a81d8599254c69d4e7532a
Sarthak691 <sarm06242@gmail.com>
2025-05-29 10:31:59 +0530

    [sarthak] Added portfolio

--- src/main/java/com/bridgelabz/cryptotracker/user/repository/CryptoAssetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.cryptotracker.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bridgelabz.cryptotracker.user.entity.CryptoAsset;
-
-public interface CryptoAssetRepository extends JpaRepository<CryptoAsset, Long> {}
